943c339ba1679c75d2efe514d34bb43c49c4b21a
jianyan <simonli1215@gmail.com>
2019-02-18 13:20:52 -0500

    :redirect url

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const https = require('https');
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/kll', (req, res) => res.send('hahah!'))
-app.get('/check', (req, res) => res.send('right'))
-
+app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/check', (req, res) => res.send('right'));
+app.get('*', (req, res) => {
+	res.redirect(req.url.slice(1,));
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
