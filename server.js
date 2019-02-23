5bc0365a8076ab915f0966f0ccd948f8e9999d26
jianyan <simonli1215@gmail.com>
2019-02-23 15:17:31 -0500

    :add console output for debugging purpose

--- server.js
+++ server.js
@@ -4,9 +4,17 @@ const https = require('https');
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'));
-app.get('/check', (req, res) => res.send('right'));
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+	console.log("Hello World");
+});
+app.get('/check', (req, res) => {
+	res.send('right');
+	console.log("connected");
+});
 app.get('*', (req, res) => {
+	console.log(`url is ${req.url}`);
 	res.redirect(req.url.slice(1,));
+	console.log(`going to ${req.url.slice(1,)}`);
 });
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
